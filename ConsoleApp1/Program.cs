cd58ec9e8119af10ea9a4c80eb2896dbb1f3dcbc
visualstudio <visualstudio@230-PC45>
2019-03-07 15:10:16 -0500

    Assignment 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+// Parminder Singh (732160)
+//CSD3354 SECTION2
+//ASSIGNMENT 2
+//MARCH 6, 2019
+
+//Ankita sharma (730066)
+//CSD3354 SECTION2
+//ASSIGNMENT 2
+//MARCH 6, 2019
+namespace DelegatesAndEvents
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[]args)
         {
+            DelegateExercises newDel = new DelegateExercises();
+            newDel.Method2();
+            Console.ReadLine();
+        }
+    }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
