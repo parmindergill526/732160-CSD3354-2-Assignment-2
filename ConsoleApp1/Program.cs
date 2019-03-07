9330c9124248cadc2b9fa53c9343a9c13ade6605
visualstudio <visualstudio@230-PC45>
2019-03-07 16:32:39 -0500

    lab9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,30 +14,17 @@ using System.Threading.Tasks;
 //CSD3354 SECTION2
 //ASSIGNMENT 2
 //MARCH 6, 2019
-namespace ConsoleApp1
+namespace DelegatesAndEvents
 {
     class program
     {
         static void Main(string[] args)
         {
-            DelegateExercises newDel = new DelegateExercises();
-            newDel.Method3();
-            Console.ReadLine();
+            
         }
     }
 
     public delegate void MyDelegate();
-    public class DelegateExercises
-    {
-
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
-    }
+    public class DelegateExercises : MyDelegate
+    { }
 }
